538998b392bec023571894cfc8ff69c6decce21d
vzalxndr <vzalxndr.reserve@gmail.com>
2026-02-26 08:10:27 +0100

    fix: open Swagger-UI for production

--- ReatsTracker.Api/Program.cs
+++ ReatsTracker.Api/Program.cs
@@ -13,11 +13,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers(); 
 
